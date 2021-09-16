0f268217fd2e8dcf7f7f4753074bab02282da2f2
s-ILIM <s-ILIM@487M113743.lwsd.k12wa.org>
2021-09-16 11:49:52 -0700

    This is my project one first commit.
    I have written the first two lines and the main method.

--- Song/src/Song.java
+++ Song/src/Song.java
@@ -1,13 +1,24 @@
-
 /*
- * Name
- * Date
- * APCS Period
+ * Isayiah Lim
+ * 9/16/2021
+ * APCS Period 2
  * Song Project
  * 
- * Program Description
+ * This program prints out the "Star Trekkin" a song by The Firm from 1987.
  */
 public class Song
 {
-    
+    //main method, prints out everything
+	public static void main(String[] args)
+	{
+		lines3and4();
+	}
+	//prints out the third and fourth line of first verse, Star Trekkin' across the universe, ect
+	public static void lines3and4()
+    {
+    	System.out.print("Star Trekkin' across the universe, ");
+    	System.out.println("On the Starship Enterprise under Captain Kirk.");
+    	System.out.print("Star Trekkin' across the universe, ");
+    	System.out.println("Only going forward 'cause we can't find reverse.");
+    }
 }
